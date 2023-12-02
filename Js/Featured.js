3efd349e6f17d4b673acee8ecdc9bcea30a6c32e
JaydenToh <s10241868@connect.np.edu.sg>
2023-12-02 21:21:43 +0800

    Update to make all websites responsive

--- Js/Featured.js
+++ Js/Featured.js
@@ -1,22 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Add event listener to the "Favourite" button
-  let addButton = document.getElementById("press");
-  addButton.addEventListener("click", function () {
-    // Get the data from the current book display
-    let bookImageElement = document.getElementById("book-image").querySelector("img");
-    let BookTitleElement = document.getElementById("BookTitle");
+  // Add event listeners to all buttons with the class "custom-button-add"
+  let addButtonElements = document.querySelectorAll(".custom-button-add");
 
-    // Get the table body
-    let tableBody = document.getElementById("student-table-body");
+  addButtonElements.forEach(function (addButton) {
+    addButton.addEventListener("click", function () {
+      // Get the data from the current book display
+      let bookContainer = addButton.closest(".book_display");
+      let bookImageElement = bookContainer.querySelector(".book-image img");
+      let BookTitleElement = bookContainer.querySelector(".book-info h3");
 
-    // Create a new row
-    let newRow = tableBody.insertRow();
+      // Get the table body
+      let tableBody = document.getElementById("student-table-body");
 
-    // Insert cells with book data
-    let imageCell = newRow.insertCell(0);
-    imageCell.innerHTML = `<img src="${bookImageElement.src}" alt="Book Image" width="50px" height="50px">`;
+      // Create a new row
+      let newRow = tableBody.insertRow();
 
-    let titleCell = newRow.insertCell(1);
-    titleCell.textContent = BookTitleElement.textContent;
+      // Insert cells with book data
+      let imageCell = newRow.insertCell(0);
+      imageCell.innerHTML = `<img src="${bookImageElement.src}" alt="Book Image" width="50px" height="50px">`;
+
+      let titleCell = newRow.insertCell(1);
+      titleCell.textContent = BookTitleElement.textContent;
+    });
   });
 });
